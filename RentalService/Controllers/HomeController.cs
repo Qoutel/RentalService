e8ac5f5c9dee95429167de499c35146a290ff3c2
Qoutel <nezzy124@outlook.com>
2022-02-23 17:38:19 +0200

    filter

--- RentalService/Controllers/HomeController.cs
+++ RentalService/Controllers/HomeController.cs
@@ -1,22 +1,43 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RentalService.Models;
+using RentalService.ViewModels;
 using System.Diagnostics;
+using System.Linq;
 
 namespace RentalService.Controllers
 {
     public class HomeController : Controller
     {
+        readonly IdentityContext _context;
+        readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UserManager<User> userManager, SignInManager<User> signInManager,
+            IdentityContext context, ApplicationDbContext dbContext, ILogger<HomeController> logger)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+            _dbContext = dbContext;
             _logger = logger;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index(bool BMW, int page = 1)
         {
-            return View();
+            int itemsOnPage = 4;
+            var vehicles = _dbContext.Vehicle.Include(m => m.Brand).Include(m => m.VehicleType)
+                .Include(m => m.VehicleClass).Include(m => m.Location).Include(m => m.FuelType).Include(m => m.Photos);
+            var count = await vehicles.CountAsync();
+            var items = await vehicles.Skip((page - 1) * itemsOnPage).Take(itemsOnPage).ToListAsync();
+            PageViewModel pageViewModel = new PageViewModel(count, page, itemsOnPage);
+            var brands = await _dbContext.VehicleBrand.ToListAsync();
+            HomePageViewModel model = new HomePageViewModel { PageViewModel = pageViewModel, Vehicles = items,
+                VehicleBrands = brands, BMW = BMW};
+
+            return View(model);
         }
 
         public IActionResult Privacy()
@@ -28,6 +49,45 @@ namespace RentalService.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult CarList()
+        {
+            List<Vehicle> cars = _dbContext.Vehicle.Include(m => m.Brand).Include(m => m.VehicleType)
+                .Include(m => m.VehicleClass).Include(m => m.Location).Include(m => m.FuelType).ToList();
+            return View(cars);
+        }
+        public IActionResult GetPartial(string[] vehicleType, string[] brand)
+        {
+            List<Vehicle> cars = _dbContext.Vehicle.Include(m => m.Brand).Include(m => m.VehicleType)
+                .Include(m => m.VehicleClass).Include(m => m.Location).Include(m => m.FuelType).ToList();
+            List<Vehicle> vehicleTypeFiltered = new List<Vehicle>();
+            List<Vehicle> brandFiltered = new List<Vehicle>();
+            if (vehicleType.Length != 0)
+            {
+                foreach (string vt in vehicleType)
+                {
+                    var filtered = cars.Where(v => v.VehicleType.Name.Contains(vt)).ToList();
+                    vehicleTypeFiltered.AddRange(filtered);
+                }
+            }
+            else
+            {
+                vehicleTypeFiltered = cars;
+            }
+            if (brand.Length != 0)
+            {
+                foreach (string b in brand)
+                {
+                    var filtered = cars.Where(v => v.Brand.Name.Contains(b)).ToList();
+                    brandFiltered.AddRange(filtered);
+                }
+            }
+            else
+            {
+                brandFiltered = cars;
+            }
+            var filteredCars = vehicleTypeFiltered.Intersect(brandFiltered);
+            return PartialView("_CarListPartial", filteredCars.ToList());
         }
     }
 }
---
+++ RentalService/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RentalService.Models;
+namespace RentalService.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<Vehicle> Vehicles { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+        public List<VehicleBrand> VehicleBrands { get; set; }
+        public bool BMW { get; set; }
+    }
+}
